5302a7d4e6d6c1566622b6a67e0bac37b739eac6
ElliotFer2000 <52060565+ElliotFer2000@users.noreply.github.com>
2023-04-13 13:55:59 -0600

    create thanks page

---
+++ page-gracias.php
@@ -0,0 +1,31 @@
+<?php
+    $hide_footer_form = true;
+    get_header();
+?>
+
+<section class="section section_full_height section_primary-accent d-flex align-items-center options_bg">
+    <div class="wrap-container_lg h-spacing-horizontal-auto">
+        <div class="row align-items-center">
+            <div class="col col-md-5 text_align-center text_align-left--md">
+                <img src="<?php echo get_theme_file_uri() ?>/images/benefits-icon@3x.png" alt="" width="200">
+            </div>
+            <div class="col-12 col-md-7">
+                <h1 class="title title_md title_hero--md title_light text_align-center text_align-left--md">Gracias Por Contactarnos</h1>
+                <p class="text text_light text_align-center text_align-left--md">Nos pondremos en contacto contigo en breve</p>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="color-divider">
+    <div class="color-divider__item color-divider__item-a"></div>
+    <div class="color-divider__item color-divider__item-b"></div>
+    <div class="color-divider__item color-divider__item-c"></div>
+    <div class="color-divider__item color-divider__item-d"></div>
+    <div class="color-divider__item color-divider__item-e"></div>
+    <div class="color-divider__item color-divider__item-f"></div>
+    <div class="color-divider__item color-divider__item-g"></div>
+</div>
+
+<?php
+    get_footer();
+?>
