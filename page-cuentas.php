f379029f2dbd2c57b3aaf1c2d425f2ca2422617b
ElliotFer2000 <52060565+ElliotFer2000@users.noreply.github.com>
2023-04-13 14:58:37 -0600

    create nueva cuenta page

---
+++ page-cuentas.php
@@ -0,0 +1,50 @@
+<?php
+    $hide_footer_form = true;
+    get_header();
+?>
+
+
+<section class="section section_pad-bottom_none section_pad-bottom_sm--md section_pad-top_none section_primary-accent img_extend-right_hand img_extend-right_hand-sm section_cuentas position_relative">
+    <div class="wrap-container-sm">
+        <div class="row justify-content-start justify-content-md-end">
+            <div class="col-12 col-md-6 col-xl-7">
+                <div class="d-flex align-items-center justify-content-center justify-content-md-start">
+                    <div class="text-center text-md-start py-lg-4">
+                        <h2 class="title_lg title_xxl--md title_hero-1--lg title_light mt-4">
+                            <span class="h-show-block">
+                                ¡Ya tienes
+                            </span>
+                            <span class="h-show-block">
+                                tu cuenta!
+                            </span>
+                        </h2>
+                        <p class="text_lg text_light h-line-height-sm-1">
+                            ¡Descarga EasyMD app ahora!
+                        </p>
+                        <a href="https://onelink.to/hjqc9u" class="btn_lg h-corner-sm btn_leading-accent btn_hover-secondary mt-3 py-3">
+                            <b class="h-font-sora text_primary-accent text_lg-1">
+                                ¡Prueba 1 mes gratis!
+                            </b>
+                        </a>
+                    </div>
+                </div>
+                <div class="d-md-none mt-5">
+                   <img src="http://easy-md.local/wp-content/themes/easymd/images/mobile-in-hand-v2@3x.png" alt="" class="d-block mx-auto h-max-width-sm h-max-width-sm-1--md h-max-width-none--md mt-4 mt-md-0">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="color-divider">
+    <div class="color-divider__item color-divider__item-a"></div>
+    <div class="color-divider__item color-divider__item-b"></div>
+    <div class="color-divider__item color-divider__item-c"></div>
+    <div class="color-divider__item color-divider__item-d"></div>
+    <div class="color-divider__item color-divider__item-e"></div>
+    <div class="color-divider__item color-divider__item-f"></div>
+    <div class="color-divider__item color-divider__item-g"></div>
+</div>
+
+<?php
+    get_footer();
+?>
